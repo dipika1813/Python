058390669fbdaddd3f114a90ef9a87233a257e04
dipika1813 <pawardipika24@gmail.com>
2026-01-19 01:17:01 +0530

    Add files via upload

---
+++ practise_set_.py
@@ -0,0 +1,40 @@
+a=4
+
+b=5
+
+c=a+b
+print(c)
+
+# 2 program to find reminder when a number is divided by z
+
+x=10
+y=5
+print('reminder when x is divided by y:',x % y)
+
+
+#3 check the type of variable assigned using input()function
+
+d=int(input('enter the value:'))
+e="dipika"
+print(type(d))
+print(type(e))
+
+#4 find a is greater than b or not? a=34 b=80
+
+a=34
+b=80
+print("a is greater than b :",a>b)
+
+
+#5 program to find an average of 2 num entered by user
+
+v=int(input("enter a num:"))
+w=int(input("enter a num:"))
+
+avg=(v+w)/2
+print(avg)
+
+#6 program to cal square of num entered by user
+
+h=int(input("enter a num:"))
+print("square of num :",h**2)
