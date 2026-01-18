fad57b8ab1ff9cbae4e0c1fbb4bccbbbae9e82fb
dipika1813 <pawardipika24@gmail.com>
2026-01-19 01:16:42 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,33 @@
+# arithmetic opertors
+a=5
+b=6
+c=a+b
+print(c)
+
+# assignment operator
+a=4-3
+print(a)
+b=5
+b+=6
+print(b)
+
+# comparison opertor
+
+f=7==5
+print(f)
+
+
+# logical operators
+
+d=True or False
+# truth table of or
+print("True or False is:" ,True or False)
+print("True or True is:" ,True or True)
+print("False or True is:" ,False or True)
+print("False or False is:" ,False or False)
+
+# truth table of and
+print("True and False is:" ,True and False)
+print("True and True is:" ,True and True)
+print("False and True is:" ,False and True)
+print("False and False is:" ,False and False)
