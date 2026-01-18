439836ee714145ee350e454064bf884cebdf1f0d
dipika1813 <pawardipika24@gmail.com>
2026-01-19 01:15:25 +0530

    Add files via upload

---
+++ problem_2.py
@@ -0,0 +1,10 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("hii dipika\n")
+engine.runAndWait()
+
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("bye  dipika\n")
+engine.runAndWait()
